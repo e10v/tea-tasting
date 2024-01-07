4dfa28be9a41aeabb762d8ac68161b9494a6dca8
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-01-07 17:04:44 +0300

    Add global config

--- src/tea_tasting/__init__.py
+++ src/tea_tasting/__init__.py
@@ -1,6 +1,8 @@
 """A Python package for statistical analysis of A/B tests."""
 # ruff: noqa: F401
+# pyright: reportUnusedImport=false
 
 from __future__ import annotations
 
-from tea_tasting._version import __version__
+from tea_tasting.config import config_context, get_config, set_config
+from tea_tasting.version import __version__
---
+++ tests/test_config.py
@@ -0,0 +1,55 @@
+# pyright: reportPrivateUsage=false
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+import tea_tasting.config
+
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+    from typing import Any
+
+
+@pytest.fixture
+def reset_config() -> Generator[None, Any, None]:  # noqa: PT004
+    old_config = tea_tasting.config._global_config.copy()
+
+    try:
+        yield
+    finally:
+        tea_tasting.config._global_config = old_config
+
+
+@pytest.mark.usefixtures("reset_config")
+def test_get_config():
+    config = tea_tasting.config.get_config()
+    assert config == tea_tasting.config._global_config
+    config["equal_var"] = not config["equal_var"]
+    assert config != tea_tasting.config._global_config
+
+    assert (
+        tea_tasting.config.get_config("equal_var") ==
+        tea_tasting.config._global_config["equal_var"]
+    )
+
+
+@pytest.mark.usefixtures("reset_config")
+def test_set_config():
+    tea_tasting.config.set_config(equal_var=True)
+    assert tea_tasting.config._global_config["equal_var"] is True
+
+    tea_tasting.config.set_config(equal_var=False)
+    assert tea_tasting.config._global_config["equal_var"] is False
+
+
+@pytest.mark.usefixtures("reset_config")
+def test_config_context():
+    old_equal_var = tea_tasting.config._global_config["equal_var"]
+
+    with tea_tasting.config.config_context(equal_var=not old_equal_var):
+        assert tea_tasting.config._global_config["equal_var"] is not old_equal_var
+
+    assert tea_tasting.config._global_config["equal_var"] is old_equal_var
