65579c354fbc9ea1ea1fa2ec5853372409dfbc4a
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-01-08 19:54:08 +0300

    Minor fix

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -5,21 +5,23 @@ import tea_tasting.datasets
 
 
 def test_sample_users_data_default():
-    users_data = tea_tasting.datasets.sample_users_data(seed=42)
+    users_data = tea_tasting.datasets.sample_users_data(size=100, seed=42)
     assert users_data.columns == ["user", "variant", "visits", "orders", "revenue"]
 
     data = users_data.to_pandas()
+    assert len(data) == 100
     assert data["user"].count() == data["user"].drop_duplicates().count()
-    assert data["variant"].drop_duplicates().count() == 2  # noqa: PLR2004
+    assert data["variant"].drop_duplicates().count() == 2
     assert data["visits"].min() > 0
     assert data["orders"].min() >= 0
-    assert data["orders"].le(data["visits"]).all()
+    assert data["orders"].le(data["visits"]).astype(int).min() == 1
     assert data["revenue"].min() >= 0
-    assert data["revenue"].gt(0).eq(data["orders"].gt(0)).all()
+    assert data["revenue"].gt(0).eq(data["orders"].gt(0)).astype(int).min() == 1
 
 
 def test_sample_users_data_covariates():
-    users_data = tea_tasting.datasets.sample_users_data(seed=42, covariates=True)
+    users_data = tea_tasting.datasets.sample_users_data(
+        size=100, seed=42, covariates=True)
     assert users_data.columns == [
         "user", "variant", "visits", "orders", "revenue",
         "visits_covariate", "orders_covariate", "revenue_covariate",
@@ -28,6 +30,10 @@ def test_sample_users_data_covariates():
     data = users_data.to_pandas()
     assert data["visits_covariate"].min() >= 0
     assert data["orders_covariate"].min() >= 0
-    assert data["orders_covariate"].le(data["visits_covariate"]).all()
+    assert data["orders_covariate"].le(data["visits_covariate"]).astype(int).min() == 1
     assert data["revenue_covariate"].min() >= 0
-    assert data["revenue_covariate"].gt(0).eq(data["orders_covariate"].gt(0)).all()
+    assert (
+        data["revenue_covariate"].gt(0)
+        .eq(data["orders_covariate"].gt(0))
+        .astype(int).min()
+    ) == 1
