70c56f1638c34af4ed3c1ecc2450c26d1a914131
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-01-08 18:15:27 +0300

    Add docstring

--- src/tea_tasting/utils.py
+++ src/tea_tasting/utils.py
@@ -11,7 +11,26 @@ def check_value(
     gt: Any = None,
     le: Any = None,
     lt: Any = None,
-):
+) -> None:
+    """Validate parameter type and value.
+
+    Args:
+        value: Parameter value.
+        name: Parameter name.
+        type_: If not `None`, parameter value must be an instance of `type_`.
+        ge: If not `None`, parameter value must be `>= ge`.
+        gt: If not `None`, parameter value must be `> gt`.
+        le: If not `None`, parameter value must be `<= le`.
+        lt: If not `None`, parameter value must be `< lt`.
+
+    Raises:
+        TypeError: Parameter value is not an instance of `type_`.
+        ValueError: any of the cases:
+        - Parameter value `< ge`.
+        - Parameter value `<= gt`.
+        - Parameter value `> le`.
+        - Parameter value `>= lt`.
+    """
     if type_ is not None and  not isinstance(value, type_):
         raise TypeError(f"{name} must be an instance of {type_}.")
 
@@ -24,5 +43,5 @@ def check_value(
     if le is not None and value > le:
         raise ValueError(f"{name} == {value}, must be <= {le}.")
 
-    if lt is not None and value <= lt:
-        raise ValueError(f"{name} == {value}, must be <= {lt}.")
+    if lt is not None and value >= lt:
+        raise ValueError(f"{name} == {value}, must be < {lt}.")
