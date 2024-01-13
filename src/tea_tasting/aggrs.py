001548ecc8451c44a4c485cf2f23298934689d79
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-01-13 16:31:43 +0300

    Minor refactoring

--- src/tea_tasting/aggrs.py
+++ src/tea_tasting/aggrs.py
@@ -60,70 +60,71 @@ class Aggregates:
         return self._cov[tea_tasting._utils.sorted_tuple(left, right)]
 
 
-class ExperimentAggregates:
-    _variant_aggregates: dict[Hashable, Aggregates]
-
-    def __init__(
-        self: Self,
-        data: Table,
-        variant_col: str,
-        mean_cols: Sequence[str],
-        var_cols: Sequence[str],
-        cov_cols: Sequence[tuple[str, str]],
-    ) -> None:
-        count_expr = {_COUNT: data.count()}
-
-        mean_expr = {
-            _MEAN.format(col): data[col].mean()
-            for col in tuple({*mean_cols, *var_cols, *itertools.chain(*cov_cols)})
-        }
-
-        mean_sq_expr = {
-            _MEAN_SQ.format(col): (data[col] * data[col]).mean()
-            for col in tuple(set(var_cols))
-        }
-
-        uniq_cov_cols = tuple({
-            tea_tasting._utils.sorted_tuple(left, right)
-            for left, right in cov_cols
-        })
-        mean_mul_expr = {
-            _MEAN_MUL.format(left, right): (data[left] * data[right]).mean()
-            for left, right in uniq_cov_cols
-        }
-
-        aggr_data = data.group_by(variant_col).aggregate(
-            **count_expr,
-            **mean_expr,
-            **mean_sq_expr,
-            **mean_mul_expr,
+    def __repr__(self: Self) -> str:
+        return (
+            f"Aggregates(count={self._count!r}, mean={self._mean!r}, "
+            f"var={self._var!r}, cov={self._cov!r})"
         )
 
-        self._variant_aggregates: dict[Hashable, Aggregates] = {}
 
-        for variant, variant_data in aggr_data.to_pandas().groupby(variant_col):
-            s = variant_data.iloc[0]
-            count = s[_COUNT]
-            mean = {col: s[_MEAN.format(col)] for col in mean_cols}
-            coef = count / (count - 1)
-            var = {
-                col: (s[_MEAN_SQ.format(col)] - s[_MEAN.format(col)]**2) * coef
-                for col in var_cols
-            }
-            cov = {
-                (left, right): (
-                    s[_MEAN_MUL.format(left, right)] -
-                    s[_MEAN.format(left)]*s[_MEAN.format(right)]
-                ) * coef
-                for left, right in uniq_cov_cols
-            }
-            self._variant_aggregates[variant] = Aggregates(
-                count=count,
-                mean=mean,
-                var=var,
-                cov=cov,
-            )
+def read_aggregates(
+    data: Table,
+    group_col: str,
+    mean_cols: Sequence[str],
+    var_cols: Sequence[str],
+    cov_cols: Sequence[tuple[str, str]],
+) -> dict[Hashable, Aggregates]:
+    count_expr = {_COUNT: data.count()}
 
+    mean_expr = {
+        _MEAN.format(col): data[col].mean()
+        for col in tuple({*mean_cols, *var_cols, *itertools.chain(*cov_cols)})
+    }
 
-    def __getitem__(self: Self, key: str) -> Aggregates:
-        return self._variant_aggregates[key]
+    mean_sq_expr = {
+        _MEAN_SQ.format(col): (data[col] * data[col]).mean()
+        for col in tuple(set(var_cols))
+    }
+
+    uniq_cov_cols = tuple({
+        tea_tasting._utils.sorted_tuple(left, right)
+        for left, right in cov_cols
+    })
+    mean_mul_expr = {
+        _MEAN_MUL.format(left, right): (data[left] * data[right]).mean()
+        for left, right in uniq_cov_cols
+    }
+
+    aggr_data = data.group_by(group_col).aggregate(
+        **count_expr,
+        **mean_expr,
+        **mean_sq_expr,
+        **mean_mul_expr,
+    )
+
+    result: dict[Hashable, Aggregates] = {}
+
+    for group, group_data in aggr_data.to_pandas().groupby(group_col):
+        s = group_data.iloc[0]
+        count = s[_COUNT]
+        mean = {col: s[_MEAN.format(col)] for col in mean_cols}
+        coef = count / (count - 1)
+        var = {
+            col: (s[_MEAN_SQ.format(col)] - s[_MEAN.format(col)]**2) * coef
+            for col in var_cols
+        }
+        cov = {
+            (left, right): (
+                s[_MEAN_MUL.format(left, right)] -
+                s[_MEAN.format(left)]*s[_MEAN.format(right)]
+            ) * coef
+            for left, right in uniq_cov_cols
+        }
+        result[group] = Aggregates(
+            count=count,
+            mean=mean,
+            var=var,
+            cov=cov,
+        )
+
+    return result
