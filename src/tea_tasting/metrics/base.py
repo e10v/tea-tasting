6911a17c2f6264ca66790b4b0390ce7c7294cd08
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-02-06 22:59:27 +0600

    Define | operation for AggrCols

--- src/tea_tasting/metrics/base.py
+++ src/tea_tasting/metrics/base.py
@@ -10,6 +10,7 @@ import ibis
 import ibis.expr.types
 import pandas as pd
 
+import tea_tasting._utils
 import tea_tasting.aggr
 
 
@@ -19,11 +20,37 @@ if TYPE_CHECKING:
 
 
 class AggrCols(NamedTuple):
-    """Columns to be aggregated for a metric analysis."""
+    """Columns to be aggregated for a metric analysis.
+
+    Attributes:
+        has_count: If True, include the sample size.
+        mean_cols: Column names for calculation of sample means.
+        var_cols: Column names for calculation of sample variances.
+        cov_cols: Pairs of column names for calculation of sample covariances.
+    """
     has_count: bool
     mean_cols: Sequence[str]
     var_cols: Sequence[str]
     cov_cols: Sequence[tuple[str, str]]
+
+    def __or__(self: AggrCols, other: AggrCols) -> AggrCols:
+        """Combine columns. Exclude duplicates.
+
+        Args:
+            other: Second objects with columns to be aggregated for a metric analysis.
+
+        Returns:
+            Combined columns to be aggregated for a metric analysis.
+        """
+        return AggrCols(
+            has_count=self.has_count or other.has_count,
+            mean_cols=tuple({*self.mean_cols, *other.mean_cols}),
+            var_cols=tuple({*self.var_cols, *other.var_cols}),
+            cov_cols=tuple({
+                tea_tasting._utils.sorted_tuple(*cols)
+                for cols in tuple(*self.cov_cols, *other.cov_cols)
+            }),
+        )
 
 
 class MetricBaseAggr(abc.ABC):
