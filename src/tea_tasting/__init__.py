feb18d5f7ef1a99147bf559c079d5163c7d32883
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-02-11 12:43:03 +0600

    Import classes and modules in root __init__.py

--- src/tea_tasting/__init__.py
+++ src/tea_tasting/__init__.py
@@ -1,6 +1,8 @@
 """A Python package for statistical analysis of A/B tests."""
 # pyright: reportUnusedImport=false
 
+from tea_tasting.aggr import Aggregates, read_aggregates
 from tea_tasting.config import config_context, get_config, set_config
 from tea_tasting.datasets import make_users_data
+from tea_tasting.metrics import MetricBase, MetricBaseAggr, MetricBaseFull
 from tea_tasting.version import __version__
