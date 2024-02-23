d1fd3e672fb73021f106c54d742bcd9e537edcff
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-02-23 19:48:12 +0600

    Define RatioOfMeans.analyze

--- src/tea_tasting/metrics/mean.py
+++ src/tea_tasting/metrics/mean.py
@@ -1,23 +1,44 @@
-"""Mean metrics."""
+"""Analysis of means of two independent samples."""
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, NamedTuple
 
 import tea_tasting._utils
+import tea_tasting.aggr
 import tea_tasting.config
 import tea_tasting.metrics.base
 
 
 if TYPE_CHECKING:
-    from typing import Literal
+    from typing import Any, Literal
+
+    import ibis.expr.types
+    import pandas as pd
+
+
+class MeansResult(NamedTuple):
+    """Result of an analysis of means.
+
+    Attributes:
+        control: _description_
+    """
+    control: float
+    treatment: float
+    effect_size: float
+    effect_size_ci_lower: float
+    effect_size_ci_upper: float
+    rel_effect_size: float
+    rel_effect_size_ci_lower: float
+    rel_effect_size_ci_upper: float
+    pvalue: float
 
 
 class RatioOfMeans(
     tea_tasting.metrics.base.MetricBaseAggregated,
     tea_tasting._utils.ReprMixin,
 ):
-    """Compares of ratios of metrics means between variants."""
+    """Compares ratios of metrics means between variants."""
 
     def __init__(  # noqa: PLR0913
         self,
@@ -94,3 +115,72 @@ class RatioOfMeans(
                 if col0 < col1
             ),
         )
+
+    def analyze(
+        self,
+        data: pd.DataFrame | ibis.expr.types.Table | dict[
+            Any, tea_tasting.aggr.Aggregates],
+        control: Any,
+        treatment: Any,
+        variant_col: str | None = None,
+    ) -> MeansResult:
+        """Analyze metric in an experiment.
+
+        Args:
+            data: Experimental data.
+            control: Control variant.
+            treatment: Treatment variant.
+            variant_col: Variant column name.
+
+        Returns:
+            Experiment result for a metric.
+        """
+        data = self.validate_aggregates(data, variant_col=variant_col)
+        contr = data[control]
+        treat = data[treatment]
+        covariate_coef = self._covariate_coef(contr + treat)
+        return self._analyze_from_stats(
+            contr_mean=contr.mean(self.numer) / contr.mean(self.denom),
+            contr_var=self._metric_var(contr, covariate_coef),
+            contr_count=contr.count(),
+            treat_mean=treat.mean(self.numer) / treat.mean(self.denom),
+            treat_var=self._metric_var(treat, covariate_coef),
+            treat_count=treat.count(),
+        )
+
+    def _analyze_from_stats(
+        self,
+        contr_mean: float,
+        contr_var: float,
+        contr_count: int,
+        treat_mean: float,
+        treat_var: float,
+        treat_count: int,
+    ) -> MeansResult:
+        ...
+
+    def _covariate_coef(self, aggr: tea_tasting.aggr.Aggregates) -> float:
+        return aggr.ratio_cov(
+            self.numer,
+            self.denom,
+            self.numer_covariate,
+            self.denom_covariate,
+        ) / aggr.ratio_var(
+            self.numer_covariate,
+            self.denom_covariate,
+        )
+
+    def _metric_var(
+        self,
+        aggr: tea_tasting.aggr.Aggregates,
+        covariate_coef: float,
+    ) -> float:
+        left_var = aggr.ratio_var(self.numer, self.denom)
+        right_var = aggr.ratio_var(self.numer_covariate, self.denom_covariate)
+        cov = aggr.ratio_cov(
+            self.numer,
+            self.denom,
+            self.numer_covariate,
+            self.denom_covariate,
+        )
+        return left_var + covariate_coef*covariate_coef*right_var - 2*covariate_coef*cov
