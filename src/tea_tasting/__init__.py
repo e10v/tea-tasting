9ce5b397b53bfc71d9ff0d85dbec88bb00300116
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-02-25 19:38:26 +0600

    Remove aggregates from root __init__

--- src/tea_tasting/__init__.py
+++ src/tea_tasting/__init__.py
@@ -1,7 +1,6 @@
 """A Python package for statistical analysis of A/B tests."""
 # pyright: reportUnusedImport=false
 
-from tea_tasting.aggr import Aggregates, read_aggregates
 from tea_tasting.config import config_context, get_config, set_config
 from tea_tasting.datasets import make_users_data
 from tea_tasting.metrics import RatioOfMeans, SimpleMean
