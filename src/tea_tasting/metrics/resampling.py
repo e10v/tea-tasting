8a209bdbe76620bb2ffc9114a9c41bbcfe353153
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-06-09 17:45:04 +0500

    Minor change + docstrings

--- src/tea_tasting/metrics/resampling.py
+++ src/tea_tasting/metrics/resampling.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 from typing import TYPE_CHECKING, NamedTuple
 
 import numpy as np
@@ -14,7 +14,6 @@ import tea_tasting.utils
 
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
     from typing import Any, Literal
 
     import numpy.typing as npt
@@ -22,6 +21,23 @@ if TYPE_CHECKING:
 
 
 class BootstrapResult(NamedTuple):
+    """Result of the analysis using bootstrap resampling.
+
+    Attributes:
+        control: Control statistic value.
+        treatment: Treatment statistic value.
+        effect_size: Absolute effect size. Difference between the two statistic values.
+        effect_size_ci_lower: Lower bound of the absolute effect size
+            confidence interval.
+        effect_size_ci_upper: Upper bound of the absolute effect size
+            confidence interval.
+        rel_effect_size: Relative effect size. Difference between the two statistic
+            values, divided by the control statistic value.
+        rel_effect_size_ci_lower: Lower bound of the relative effect size
+            confidence interval.
+        rel_effect_size_ci_upper: Upper bound of the relative effect size
+            confidence interval.
+    """
     control: float
     treatment: float
     effect_size: float
@@ -32,8 +48,8 @@ class BootstrapResult(NamedTuple):
     rel_effect_size_ci_upper: float
 
 
-class Bootstrap(MetricBaseGranular[BootstrapResult]):
-    def __init__(
+class Bootstrap(MetricBaseGranular[BootstrapResult]):  # noqa: D101
+    def __init__(  # noqa: PLR0913
         self,
         columns: str | Sequence[str],
         statistic: Callable[..., npt.NDArray[np.number[Any]]],
@@ -45,6 +61,75 @@ class Bootstrap(MetricBaseGranular[BootstrapResult]):
         batch: int | None = None,
         random_state: int | np.random.Generator | np.random.SeedSequence | None = None,
     ) -> None:
+        """Metric for analysis of a statistic using confidence interval.
+
+        Args:
+            columns: Names of the columns to be used in the analysis.
+            statistic: Statistic. It must be a vectorized callable
+                that accepts a NumPy array as the first argument and returns
+                the resulting statistic.
+                It must also accept a keyword argument `axis` and be vectorized
+                to compute the statistic along the provided axis.
+            alternative: Alternative hypothesis.
+            confidence_level: Confidence level for the confidence interval.
+            n_resamples: The number of resamples performed to form
+                the bootstrap distribution of the statistic.
+            method: Whether to return the "percentile" bootstrap confidence
+                interval (`"percentile"`), the "basic" (AKA "reverse") bootstrap
+                confidence interval (`"basic"`), or the bias-corrected
+                and accelerated bootstrap confidence interval (`"bca"`).
+            batch: The number of resamples to process in each vectorized call
+                to statistic. Memory usage is O(`batch * n`), where `n` is
+                the sample size. Default is `None`, in which case `batch = n_resamples`
+                (or `batch = max(n_resamples, n)` for method="bca").
+            random_state: Pseudorandom number generator state used
+                to generate resamples.
+
+        Notes:
+            If `columns` is a sequence of strings, then the sample passed
+            to the statistic callable contains an extra dimension in the first axis.
+            See examples below.
+
+        Examples:
+            ```python
+            import numpy as np
+            import tea_tasting as tt
+
+
+            experiment = tt.Experiment(
+                orders_per_user=tt.Bootstrap("orders", np.mean, random_state=42),
+            )
+
+            data = tt.make_users_data(seed=42)
+            result = experiment.analyze(data)
+            print(result)
+            #>          metric control treatment rel_effect_size rel_effect_size_ci pvalue
+            #> orders_per_user   0.530     0.573            8.0%       [-1.8%, 19%]      -
+            ```
+
+            With multiple columns:
+
+            ```python
+            def ratio_of_means(sample, axis):
+                means = np.mean(sample, axis=axis)
+                return means[0] / means[1]
+
+            experiment = tt.Experiment(
+                orders_per_session=tt.Bootstrap(
+                    ("orders", "sessions"),
+                    ratio_of_means,
+                    random_state=42,
+                ),
+            )
+
+            data = tt.make_users_data(seed=42)
+            result = experiment.analyze(data)
+            print(result)
+            #>             metric control treatment rel_effect_size rel_effect_size_ci pvalue
+            #> orders_per_session   0.266     0.289            8.8%      [-0.61%, 20%]      -
+            ```
+        """  # noqa: E501
+        tea_tasting.utils.check_scalar(columns, "columns", typ=str | Sequence)
         if not isinstance(columns, str):
             for col in columns:
                 tea_tasting.utils.check_scalar(col, "column", typ=str)
@@ -82,6 +167,7 @@ class Bootstrap(MetricBaseGranular[BootstrapResult]):
 
     @property
     def cols(self) -> Sequence[str]:
+        """Columns to be fetched for a metric analysis."""
         if isinstance(self.columns, str):
             return (self.columns,)
         return self.columns
@@ -92,6 +178,15 @@ class Bootstrap(MetricBaseGranular[BootstrapResult]):
         control: pd.DataFrame,
         treatment: pd.DataFrame,
     ) -> BootstrapResult:
+        """Analyze metric in an experiment using granular data.
+
+        Args:
+            control: Control data.
+            treatment: Treatment data.
+
+        Returns:
+            Analysis result.
+        """
         def statistic(
             contr: npt.NDArray[np.number[Any]],
             treat: npt.NDArray[np.number[Any]],
