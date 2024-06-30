ded58af501c11248ff5734f1568661465a5006a6
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2024-06-30 16:50:24 +0500

    Analysis of power for RatioOfMeans

--- src/tea_tasting/metrics/mean.py
+++ src/tea_tasting/metrics/mean.py
@@ -4,18 +4,28 @@
 from __future__ import annotations
 
 import math
-from typing import TYPE_CHECKING, NamedTuple
+from typing import TYPE_CHECKING, NamedTuple, overload
 
+import scipy.optimize
 import scipy.stats
 
 import tea_tasting.aggr
 import tea_tasting.config
-from tea_tasting.metrics.base import AggrCols, MetricBaseAggregated
+from tea_tasting.metrics.base import (
+    AggrCols,
+    MetricBaseAggregated,
+    PowerBaseAggregated,
+    PowerParameter,
+)
 import tea_tasting.utils
 
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
     from typing import Literal
+
+
+MAX_ITER = 100
 
 
 class MeanResult(NamedTuple):
@@ -50,7 +60,7 @@ class MeanResult(NamedTuple):
     statistic: float
 
 
-class RatioOfMeans(MetricBaseAggregated[MeanResult]):  # noqa: D101
+class RatioOfMeans(MetricBaseAggregated[MeanResult], PowerBaseAggregated):  # noqa: D101
     def __init__(  # noqa: PLR0913
         self,
         numer: str,
@@ -263,6 +273,72 @@ class RatioOfMeans(MetricBaseAggregated[MeanResult]):  # noqa: D101
         )
 
 
+    def solve_power_from_aggregates(
+        self,
+        data: tea_tasting.aggr.Aggregates,
+        parameter: PowerParameter = "power",
+    ) -> float | int:
+        """Solve for a parameter of the power of a test.
+
+        Args:
+            data: Sample data.
+            parameter: Parameter name.
+
+        Returns:
+            The value of the parameter that was set in the `parameter` argument.
+        """
+        tea_tasting.utils.check_scalar(
+            parameter,
+            "parameter",
+            in_={"power", "effect_size", "rel_effect_size", "n_obs"},
+        )
+
+        data = data.with_zero_div()
+        covariate_coef = self._covariate_coef(data)
+        covariate_mean = data.mean(self.numer_covariate) / data.mean(
+            self.denom_covariate)
+        metric_mean = self._metric_mean(data, covariate_coef, covariate_mean)
+
+        n_obs = None
+        effect_size = None
+        power = None
+
+        if parameter in {"power", "n_obs"}:
+            if self.effect_size is None and self.rel_effect_size is None:
+                raise ValueError(
+                    "Both `effect_size` and `rel_effect_size` are `None`. "
+                    "One of them should be defined.",
+                )
+            if self.effect_size is not None and self.rel_effect_size is not None:
+                raise ValueError(
+                    "Both `effect_size` and `rel_effect_size` are not `None`. "
+                    "Only one of them should be defined.",
+                )
+            effect_size = (
+                self.effect_size if self.rel_effect_size is None
+                else self.rel_effect_size * metric_mean
+            )
+
+        if parameter in {"power", "effect_size", "rel_effect_size"}:
+            n_obs = data.count() if self.n_obs is None else self.n_obs
+
+        if parameter in {"effect_size", "rel_effect_size", "n_obs"}:
+            power = self.power
+
+        parameter_value = self._solve_power_from_stats(
+            sample_var=self._metric_var(data, covariate_coef),
+            sample_count=n_obs,
+            effect_size=effect_size,
+            power=power,
+        )
+
+        if parameter == "rel_effect_size":
+            return parameter_value / metric_mean
+        if parameter == "n_obs":
+            return math.ceil(parameter_value)
+        return parameter_value
+
+
     def _covariate_coef(self, aggr: tea_tasting.aggr.Aggregates) -> float:
         covariate_var = aggr.ratio_var(self.numer_covariate, self.denom_covariate)
         if covariate_var == 0:
@@ -314,13 +390,13 @@ class RatioOfMeans(MetricBaseAggregated[MeanResult]):  # noqa: D101
         treat_var: float,
         treat_count: int,
     ) -> MeanResult:
-        scale, distr = self._scale_and_distr(
+        scale, distr, _ = self._scale_and_distr(
             contr_var=contr_var,
             contr_count=contr_count,
             treat_var=treat_var,
             treat_count=treat_count,
         )
-        log_scale, log_distr = self._scale_and_distr(
+        log_scale, log_distr, _ = self._scale_and_distr(
             contr_var=contr_var / contr_mean / contr_mean,
             contr_count=contr_count,
             treat_var=treat_var / treat_mean / treat_mean,
@@ -369,13 +445,102 @@ class RatioOfMeans(MetricBaseAggregated[MeanResult]):  # noqa: D101
         )
 
 
+    def _solve_power_from_stats(
+        self,
+        sample_var: float,
+        sample_count: int | None = None,
+        effect_size: float | None = None,
+        power: float | None = None,
+    ) -> float | int:
+        if power is None and effect_size is not None and sample_count is not None:
+            return self._power_from_stats(
+                sample_var=sample_var,
+                sample_count=sample_count,
+                effect_size=effect_size,
+            )
+
+        if power is not None and effect_size is None and sample_count is not None:
+            def fn(x: float | int) -> float:
+                return power - self._power_from_stats(
+                    sample_var=sample_var,
+                    sample_count=sample_count,
+                    effect_size=x,
+                )
+            sign = -1 if self.alternative == "less" else 1
+            other_bound = _find_boundary(
+                fn,
+                sign * 10 * math.sqrt(sample_var / sample_count),
+                sign * 10,
+            )
+            lower_bound, upper_bound = sorted((0, other_bound))
+
+        if power is not None and effect_size is not None and sample_count is None:
+            def fn(x: float | int) -> float:
+                return power - self._power_from_stats(
+                    sample_var=sample_var,
+                    sample_count=x,
+                    effect_size=effect_size,
+                )
+            lower_bound = 3
+            upper_bound = _find_boundary(fn, 10)
+
+        return scipy.optimize.brentq(fn, lower_bound, upper_bound, maxiter=MAX_ITER)  # type: ignore
+
+
+    def _power_from_stats(
+        self,
+        sample_var: float,
+        sample_count: int | float,
+        effect_size: float,
+    ) -> float:
+        _, null_distr, alt_distr = self._scale_and_distr(
+            contr_var=sample_var,
+            contr_count=sample_count / (1 + self.ratio),
+            treat_var=sample_var,
+            treat_count=sample_count * self.ratio / (1 + self.ratio),
+            effect_size=effect_size,
+        )
+        if self.alternative == "greater":
+            stat_critical = null_distr.isf(self.alpha)
+            return alt_distr.sf(stat_critical)
+        if self.alternative == "less":
+            stat_critical = null_distr.ppf(self.alpha)
+            return alt_distr.cdf(stat_critical)
+        # two-sided
+        stat_critical = null_distr.isf(self.alpha / 2)
+        return alt_distr.cdf(-stat_critical) + alt_distr.sf(stat_critical)
+
+
+    @overload
     def _scale_and_distr(
         self,
         contr_var: float,
-        contr_count: int,
+        contr_count: int | float,
         treat_var: float,
-        treat_count: int,
-    ) -> tuple[float, scipy.stats.rv_frozen]:
+        treat_count: int | float,
+        effect_size: None = None,
+    ) -> tuple[float, scipy.stats.rv_frozen, None]:
+        ...
+
+    @overload
+    def _scale_and_distr(
+        self,
+        contr_var: float,
+        contr_count: int | float,
+        treat_var: float,
+        treat_count: int | float,
+        effect_size: float,
+    ) -> tuple[float, scipy.stats.rv_frozen, scipy.stats.rv_frozen]:
+        ...
+
+    def _scale_and_distr(
+        self,
+        contr_var: float,
+        contr_count: int | float,
+        treat_var: float,
+        treat_count: int | float,
+        effect_size: float | None = None,
+    ) -> tuple[float, scipy.stats.rv_frozen, scipy.stats.rv_frozen | None]:
         if self.equal_var:
             pooled_var = (
                 (contr_count - 1)*contr_var + (treat_count - 1)*treat_var
@@ -394,11 +559,33 @@ class RatioOfMeans(MetricBaseAggregated[MeanResult]):  # noqa: D101
                     contr_mean_var**2 / (contr_count - 1)
                     + treat_mean_var**2 / (treat_count - 1)
                 )
-            distr = scipy.stats.t(df=df)
+            null_distr = scipy.stats.t(df=df)
+            alt_distr = None if effect_size is None else scipy.stats.nct(
+                df=df, nc=effect_size / scale)
         else:
-            distr = scipy.stats.norm()
+            null_distr = scipy.stats.norm()
+            alt_distr = None if effect_size is None else scipy.stats.norm(
+                loc=effect_size / scale)
 
-        return scale, distr
+        return scale, null_distr, alt_distr
+
+
+def _find_boundary(
+    fn: Callable[[float | int], float],
+    init: float | int,
+    mult: float | int = 10,
+) -> float:
+    b = init
+    i = 0
+    while fn(b) > 0:
+        b *= mult
+        i += 1
+        if i == MAX_ITER:
+            raise RuntimeError(
+                "Cannot find parameter boundaries. "
+                "Maximum number of iterations is reached.",
+            )
+    return b
 
 
 class Mean(RatioOfMeans):  # noqa: D101
