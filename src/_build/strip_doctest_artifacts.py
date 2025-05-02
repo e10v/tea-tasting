4675b42a5f323ad54835e6cad94f5adf6f0cf8e2
Evgeny Ivanov <14168452+e10v@users.noreply.github.com>
2025-05-02 13:30:30 +0500

    build: refactor the strip_doctest_artifacts extension for mkdocs (#162)

--- src/_build/strip_doctest_artifacts.py
+++ src/_build/strip_doctest_artifacts.py
@@ -9,30 +9,22 @@ import markdown.extensions
 import markdown.preprocessors
 
 
+_doctest_pattern = re.compile(r"<BLANKLINE>|\s+# doctest:.*")
 class StripDoctestArtifactsPreprocessor(markdown.preprocessors.Preprocessor):
     """A preprocessor that removes doctest artifacts."""
-
     def run(self, lines: list[str]) -> list[str]:
         """Run the preprocessor."""
-        return [_strip(line) for line in lines]
-
-
-_doctest_pattern = re.compile(r"\s+# doctest:.*")
-def _strip(line: str) -> str:
-    return _doctest_pattern.sub("", line.replace("<BLANKLINE>", ""))
-
+        return [_doctest_pattern.sub("", line) for line in lines]
 
 class StripDoctestArtifactsExtension(markdown.extensions.Extension):
     """An extension that registers the preprocessor."""
-
     def extendMarkdown(self, md: markdown.Markdown) -> None:
         """Register the preprocessor."""
         md.preprocessors.register(
             StripDoctestArtifactsPreprocessor(md),
-            "blankline_strip_preprocessor",
+            "doctest_strip_preprocessor",
             175,
         )
-
 
 def makeExtension(**kwargs: dict[str, object]) -> StripDoctestArtifactsExtension:
     """A factory function for the extension, required by Python-Markdown."""
