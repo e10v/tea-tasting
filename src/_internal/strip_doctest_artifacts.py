d3bb66f2d6feba943a615624c97acf2d6f6c4f58
Evgeny Ivanov <14168452+e10v@users.noreply.github.com>
2025-05-03 22:47:36 +0500

    chore: minor cleanup in an internal module (#164)

--- src/_internal/strip_doctest_artifacts.py
+++ src/_internal/strip_doctest_artifacts.py
@@ -9,23 +9,19 @@ import markdown.extensions
 import markdown.preprocessors
 
 
-_doctest_pattern = re.compile(r"<BLANKLINE>|\s+# doctest:.*")
+RE_DOCTEST = re.compile(r"<BLANKLINE>|\s+# doctest:.*")
+
 class StripDoctestArtifactsPreprocessor(markdown.preprocessors.Preprocessor):
-    """A preprocessor that removes doctest artifacts."""
     def run(self, lines: list[str]) -> list[str]:
-        """Run the preprocessor."""
-        return [_doctest_pattern.sub("", line) for line in lines]
+        return [RE_DOCTEST.sub("", line) for line in lines]
 
 class StripDoctestArtifactsExtension(markdown.extensions.Extension):
-    """An extension that registers the preprocessor."""
     def extendMarkdown(self, md: markdown.Markdown) -> None:
-        """Register the preprocessor."""
         md.preprocessors.register(
             StripDoctestArtifactsPreprocessor(md),
-            "doctest_strip_preprocessor",
+            "strip_doctest_artifacts",
             175,
         )
 
 def makeExtension(**kwargs: dict[str, object]) -> StripDoctestArtifactsExtension:
-    """A factory function for the extension, required by Python-Markdown."""
     return StripDoctestArtifactsExtension(**kwargs)
