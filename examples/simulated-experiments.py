a45c05e2c1151a3a3ea97b20c3d5a03fe851498e
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2025-05-04 02:48:08 +0500

    docs: fix indent

--- examples/simulated-experiments.py
+++ examples/simulated-experiments.py
@@ -17,26 +17,26 @@ app = marimo.App()
 def _(mo):
     mo.md(
         r"""
-    # Simulated experiments
+        # Simulated experiments
 
-    ## Intro
+        ## Intro
 
-    In tea-tasting, you can run multiple simulated A/A or A/B tests. In each simulation, tea-tasting splits the data into control and treatment groups and can optionally modify the treatment data. A simulation without changing the treatment data is called an A/A test. A/A tests are useful for identifying potential issues before conducting the actual A/B test. Simulations where the treatment data is modified are useful for power analysis, especially when you need a specific uplift distribution or when an analytical solution isn't feasible.
+        In tea-tasting, you can run multiple simulated A/A or A/B tests. In each simulation, tea-tasting splits the data into control and treatment groups and can optionally modify the treatment data. A simulation without changing the treatment data is called an A/A test. A/A tests are useful for identifying potential issues before conducting the actual A/B test. Simulations where the treatment data is modified are useful for power analysis, especially when you need a specific uplift distribution or when an analytical solution isn't feasible.
 
-    /// admonition | Note
+        /// admonition | Note
 
-    This guide uses [Polars](https://github.com/pola-rs/polars) and [tqdm](https://github.com/tqdm/tqdm). Install these packages in addition to tea-tasting to reproduce the examples:
+        This guide uses [Polars](https://github.com/pola-rs/polars) and [tqdm](https://github.com/tqdm/tqdm). Install these packages in addition to tea-tasting to reproduce the examples:
 
-    ```bash
-    uv pip install polars tqdm
-    ```
+        ```bash
+        uv pip install polars tqdm
+        ```
 
-    ///
+        ///
 
-    ## Running A/A tests
+        ## Running A/A tests
 
-    First, let's prepare the data without any uplift and drop the `"variant"` column.
-    """
+        First, let's prepare the data without any uplift and drop the `"variant"` column.
+        """
     )
     return
 
@@ -56,7 +56,11 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""To run A/A tests, first define the metrics for the experiment, then call the [`simulate`](https://tea-tasting.e10v.me/api/experiment/#tea_tasting.experiment.Experiment.simulate) method, providing the data and the number of simulations as arguments.""")
+    mo.md(
+        r"""
+        To run A/A tests, first define the metrics for the experiment, then call the [`simulate`](https://tea-tasting.e10v.me/api/experiment/#tea_tasting.experiment.Experiment.simulate) method, providing the data and the number of simulations as arguments.
+        """
+    )
     return
 
 
@@ -80,25 +84,19 @@ def _(data, tt):
         "rel_effect_size_ci_upper",
         "pvalue",
     )
-    return experiment, results, results_data
-
-
-@app.cell
-def _(results):
-    results
-    return
+    return experiment, results_data
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
-    The `simulate` method accepts data in the same formats as the `analyze` method. Internally, however, it converts the data to a PyArrow Table before running the simulations.
+        The `simulate` method accepts data in the same formats as the `analyze` method. Internally, however, it converts the data to a PyArrow Table before running the simulations.
 
-    The method returns an instance of the [`SimulationResults`](https://tea-tasting.e10v.me/api/experiment/#tea_tasting.experiment.SimulationResults) class, which contains the results of all simulations for all metrics. The resulting object provides serialization methods to those of the experiment result, including `to_dicts`, `to_arrow`, `to_pandas`, `to_polars`, `to_pretty_dicts`, `to_string`, `to_html`.
+        The method returns an instance of the [`SimulationResults`](https://tea-tasting.e10v.me/api/experiment/#tea_tasting.experiment.SimulationResults) class, which contains the results of all simulations for all metrics. The resulting object provides serialization methods to those of the experiment result, including `to_dicts`, `to_arrow`, `to_pandas`, `to_polars`, `to_pretty_dicts`, `to_string`, `to_html`.
 
-    For instance, we can now calculate the proportion of rejected null hypotheses, using various significance levels (`alpha`). In A/A tests, it estimates the type I error rate.
-    """
+        For instance, we can now calculate the proportion of rejected null hypotheses, using various significance levels (`alpha`). In A/A tests, it estimates the type I error rate.
+        """
     )
     return
 
@@ -121,12 +119,12 @@ def _(pl, results_data):
 def _(mo):
     mo.md(
         r"""
-    100 simulations, as in the example above, produce a very rough estimate. In practice, a larger number of simulations, such as the default `10_000`, is recommended.
+        100 simulations, as in the example above, produce a very rough estimate. In practice, a larger number of simulations, such as the default `10_000`, is recommended.
 
-    ## Simulating experiments with treatment
+        ## Simulating experiments with treatment
 
-    To simulate experiments with treatment, define a treatment function that takes data in the form of a PyArrow Table and returns a PyArrow Table with the modified data:
-    """
+        To simulate experiments with treatment, define a treatment function that takes data in the form of a PyArrow Table and returns a PyArrow Table with the modified data:
+        """
     )
     return
 
@@ -151,14 +149,14 @@ def _(data, experiment, null_rejected):
 def _(mo):
     mo.md(
         r"""
-    In the example above, we've defined a function that increases the number of orders and the revenue by 10%. For these metrics, the proportion of rejected null hypotheses is an estimate of statistical power.
+        In the example above, we've defined a function that increases the number of orders and the revenue by 10%. For these metrics, the proportion of rejected null hypotheses is an estimate of statistical power.
 
-    ## Using a function instead of static data
+        ## Using a function instead of static data
 
-    You can use a function instead of static data to generate input dynamically. The function should take an instance of `numpy.random.Generator` as a parameter named `seed` and return experimental data in any format supported by tea-tasting.
+        You can use a function instead of static data to generate input dynamically. The function should take an instance of `numpy.random.Generator` as a parameter named `seed` and return experimental data in any format supported by tea-tasting.
 
-    As an example, let's use the `make_users_data` function.
-    """
+        As an example, let's use the `make_users_data` function.
+        """
     )
     return
 
@@ -174,12 +172,12 @@ def _(experiment, null_rejected, tt):
 def _(mo):
     mo.md(
         r"""
-    On each iteration, tea-tasting calls `make_users_data` with a new `seed` and uses the returned data for the analysis of the experiment. The data returned by `make_users_data` already contains the `"variant"` column, so tea-tasting reuses that split. By default, `make_users_data` also adds the treatment uplift, and you can see it in the proportion of rejected null hypotheses.
+        On each iteration, tea-tasting calls `make_users_data` with a new `seed` and uses the returned data for the analysis of the experiment. The data returned by `make_users_data` already contains the `"variant"` column, so tea-tasting reuses that split. By default, `make_users_data` also adds the treatment uplift, and you can see it in the proportion of rejected null hypotheses.
 
-    ## Tracking progress
+        ## Tracking progress
 
-    To track the progress of simulations with [`tqdm`](https://github.com/tqdm/tqdm), use the `progress` parameter.
-    """
+        To track the progress of simulations with [`tqdm`](https://github.com/tqdm/tqdm), use the `progress` parameter.
+        """
     )
     return
 
@@ -196,16 +194,16 @@ def _(data, experiment):
 def _(mo):
     mo.md(
         r"""
-    ## Parallel execution
+        ## Parallel execution
 
-    /// admonition | Note
+        /// admonition | Note
 
-    The code below won't work in the [marimo online playground](https://docs.marimo.io/guides/publishing/playground/) as it relies on the `multiprocessing` module which is currently [not supported](https://docs.marimo.io/guides/wasm/#limitations) by WASM notebooks. [WASM notebooks](https://docs.marimo.io/guides/wasm/) are the marimo notebooks that run entirely in the browser.
+        The code below won't work in the [marimo online playground](https://docs.marimo.io/guides/publishing/playground/) as it relies on the `multiprocessing` module which is currently [not supported](https://docs.marimo.io/guides/wasm/#limitations) by WASM notebooks. [WASM notebooks](https://docs.marimo.io/guides/wasm/) are the marimo notebooks that run entirely in the browser.
 
-    ///
+        ///
 
-    To speed up simulations and run them in parallel, use the `map_` parameter with an alternative mapping function.
-    """
+        To speed up simulations and run them in parallel, use the `map_` parameter with an alternative mapping function.
+        """
     )
     return
 
@@ -230,10 +228,10 @@ def _(data, experiment, tqdm, treat):
 def _(mo):
     mo.md(
         r"""
-    As an alternative to [`concurrent.futures.ProcessPoolExecutor`](https://docs.python.org/3/library/concurrent.futures.html#processpoolexecutor), you can use the `map`, `imap`, or `imap_unordered` methods of [`multiprocessing.pool.Pool`](https://docs.python.org/3/library/multiprocessing.html#module-multiprocessing.pool).
+        As an alternative to [`concurrent.futures.ProcessPoolExecutor`](https://docs.python.org/3/library/concurrent.futures.html#processpoolexecutor), you can use the `map`, `imap`, or `imap_unordered` methods of [`multiprocessing.pool.Pool`](https://docs.python.org/3/library/multiprocessing.html#module-multiprocessing.pool).
 
-    It's also possible to run simulations on a distributed [Dask](https://distributed.dask.org/en/stable/api.html#distributed.Client.map) or [Ray](https://docs.ray.io/en/latest/ray-core/api/doc/ray.util.ActorPool.map.html#ray.util.ActorPool.map) cluster.
-    """
+        It's also possible to run simulations on a distributed [Dask](https://distributed.dask.org/en/stable/api.html#distributed.Client.map) or [Ray](https://docs.ray.io/en/latest/ray-core/api/doc/ray.util.ActorPool.map.html#ray.util.ActorPool.map) cluster.
+        """
     )
     return
 
