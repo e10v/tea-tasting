5f00b1c37d3f105591d432119a2cc1eafe895f45
Evgeny Ivanov <14168452+e10v@users.noreply.github.com>
2025-05-09 20:01:40 +0500

    feat: make global config thread-safe and async-safe (#187)

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,46 +8,44 @@ import tea_tasting.config
 
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Iterator
 
 
 @pytest.fixture
-def reset_config() -> Generator[None, object, None]:
-    old_config = tea_tasting.config._global_config.copy()
-
+def reset_config() -> Iterator[None]:
     try:
         yield
     finally:
-        tea_tasting.config._global_config = old_config
+        tea_tasting.config._config_var.set(tea_tasting.config._DEFAULT_CONFIG.copy())
 
 
 @pytest.mark.usefixtures("reset_config")
 def test_get_config():
     config = tea_tasting.config.get_config()
-    assert config == tea_tasting.config._global_config
+    assert config == tea_tasting.config._config_var.get()
     config["equal_var"] = not config["equal_var"]
-    assert config != tea_tasting.config._global_config
+    assert config != tea_tasting.config._config_var.get()
 
     assert (
         tea_tasting.config.get_config("equal_var") ==
-        tea_tasting.config._global_config["equal_var"]
+        tea_tasting.config._config_var.get()["equal_var"]
     )
 
 
 @pytest.mark.usefixtures("reset_config")
 def test_set_config():
     tea_tasting.config.set_config(equal_var=True)
-    assert tea_tasting.config._global_config["equal_var"] is True
+    assert tea_tasting.config._config_var.get()["equal_var"] is True
 
     tea_tasting.config.set_config(equal_var=False)
-    assert tea_tasting.config._global_config["equal_var"] is False
+    assert tea_tasting.config._config_var.get()["equal_var"] is False
 
 
 @pytest.mark.usefixtures("reset_config")
 def test_config_context():
-    old_equal_var = tea_tasting.config._global_config["equal_var"]
+    old_equal_var = tea_tasting.config._config_var.get()["equal_var"]
 
     with tea_tasting.config.config_context(equal_var=not old_equal_var):
-        assert tea_tasting.config._global_config["equal_var"] is not old_equal_var
+        assert tea_tasting.config._config_var.get() is not old_equal_var
 
-    assert tea_tasting.config._global_config["equal_var"] is old_equal_var
+    assert tea_tasting.config._config_var.get()["equal_var"] is old_equal_var
