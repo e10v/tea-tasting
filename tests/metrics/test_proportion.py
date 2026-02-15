f0b36c8b3238a44f51f8ddf84a33343c681678f2
Evgeny Ivanov <14168452+e10v@users.noreply.github.com>
2026-02-15 13:36:45 +0300

    feat: add confidence intervals for proportion metric with normal approximation (#241)

--- tests/metrics/test_proportion.py
+++ tests/metrics/test_proportion.py
@@ -42,6 +42,7 @@ def test_proportion_init_default() -> None:
     assert metric.column == "a"
     assert metric.method == "auto"
     assert metric.alternative == "two-sided"
+    assert metric.confidence_level == tea_tasting.config.get_config("confidence_level")
     assert metric.correction == tea_tasting.config.get_config("correction")
     assert metric.equal_var is True
 
@@ -50,18 +51,21 @@ def test_proportion_init_custom() -> None:
         "b",
         method="norm",
         alternative="greater",
+        confidence_level=0.8,
         correction=False,
         equal_var=True,
     )
     assert metric.column == "b"
     assert metric.method == "norm"
     assert metric.alternative == "greater"
+    assert metric.confidence_level == 0.8
     assert metric.correction is False
     assert metric.equal_var is True
 
 def test_proportion_init_config() -> None:
     with tea_tasting.config.config_context(
         alternative="less",
+        confidence_level=0.8,
         correction=False,
         equal_var=False,
     ):
@@ -69,6 +73,7 @@ def test_proportion_init_config() -> None:
     assert metric.column == "a"
     assert metric.method == "auto"
     assert metric.alternative == "less"
+    assert metric.confidence_level == 0.8
     assert metric.correction is False
     assert metric.equal_var is True
 
@@ -104,16 +109,18 @@ def test_proportion_analyze_auto() -> None:
         ))
         metric.analyze(data, 0, 1, variant="variant")
         mock.assert_called_once()
-    with unittest.mock.patch(
-        "tea_tasting.metrics.proportion._2sample_proportion_ztest",
-    ) as mock:
-        mock.return_value = 0.1
+
+    with unittest.mock.patch("scipy.stats.barnard_exact") as mock:
         data = append_has_order(tea_tasting.datasets.make_users_data(
             seed=42,
             n_users=tea_tasting.metrics.proportion._MAX_EXACT_THRESHOLD,
         ))
-        metric.analyze(data, 0, 1, variant="variant")
-        mock.assert_called_once()
+        result = metric.analyze(data, 0, 1, variant="variant")
+        mock.assert_not_called()
+        assert not math.isnan(result.effect_size_ci_lower)
+        assert not math.isnan(result.effect_size_ci_upper)
+        assert not math.isnan(result.rel_effect_size_ci_lower)
+        assert not math.isnan(result.rel_effect_size_ci_upper)
 
 def test_proportion_analyze_barnard(
     data_aggr: dict[object, tea_tasting.aggr.Aggregates],
@@ -123,7 +130,11 @@ def test_proportion_analyze_barnard(
     assert result.control == pytest.approx(0.3584905660377358)
     assert result.treatment == pytest.approx(0.2765957446808511)
     assert result.effect_size == pytest.approx(-0.08189482135688475)
+    assert math.isnan(result.effect_size_ci_lower)
+    assert math.isnan(result.effect_size_ci_upper)
     assert result.rel_effect_size == pytest.approx(-0.22844344904815217)
+    assert math.isnan(result.rel_effect_size_ci_lower)
+    assert math.isnan(result.rel_effect_size_ci_upper)
     assert result.pvalue == pytest.approx(0.530637102766593)
 
 def test_proportion_analyze_barnard_less_pooled(
@@ -201,6 +212,10 @@ def test_proportion_analyze_norm(
 ) -> None:
     metric = tea_tasting.metrics.proportion.Proportion("has_order", method="norm")
     result = metric.analyze(data_aggr, 0, 1, variant="variant")
+    assert result.effect_size_ci_lower == pytest.approx(-0.2851524037206986)
+    assert result.effect_size_ci_upper == pytest.approx(0.12136276100692911)
+    assert result.rel_effect_size_ci_lower == pytest.approx(-0.5884358933441386)
+    assert result.rel_effect_size_ci_upper == pytest.approx(0.44643204227351263)
     assert result.pvalue == pytest.approx(0.5083165530405196)
 
 def test_proportion_analyze_norm_unpooled(
@@ -209,6 +224,10 @@ def test_proportion_analyze_norm_unpooled(
     metric = tea_tasting.metrics.proportion.Proportion(
         "has_order", method="norm", equal_var=False)
     result = metric.analyze(data_aggr, 0, 1, variant="variant")
+    assert result.effect_size_ci_lower == pytest.approx(-0.28368859581157896)
+    assert result.effect_size_ci_upper == pytest.approx(0.11989895309780946)
+    assert result.rel_effect_size_ci_lower == pytest.approx(-0.5939973223152473)
+    assert result.rel_effect_size_ci_upper == pytest.approx(0.46624528367997886)
     assert result.pvalue == pytest.approx(0.5049066155125612)
 
 def test_proportion_analyze_norm_greater(
@@ -217,6 +236,10 @@ def test_proportion_analyze_norm_greater(
     metric = tea_tasting.metrics.proportion.Proportion(
         "has_order", method="norm", alternative="greater")
     result = metric.analyze(data_aggr, 1, 0, variant="variant")
+    assert result.effect_size_ci_lower == pytest.approx(-0.0919114073864812)
+    assert result.effect_size_ci_upper == 1
+    assert result.rel_effect_size_ci_lower == pytest.approx(-0.25445686244444576)
+    assert result.rel_effect_size_ci_upper == float("inf")
     assert result.pvalue == pytest.approx(0.2541582765202598)
 
 def test_proportion_analyze_norm_less(
@@ -225,6 +248,10 @@ def test_proportion_analyze_norm_less(
     metric = tea_tasting.metrics.proportion.Proportion(
         "has_order", method="norm", alternative="less")
     result = metric.analyze(data_aggr, 0, 1, variant="variant")
+    assert result.effect_size_ci_lower == -1
+    assert result.effect_size_ci_upper == pytest.approx(0.0919114073864812)
+    assert result.rel_effect_size_ci_lower == -1
+    assert result.rel_effect_size_ci_upper == pytest.approx(0.3192513062736768)
     assert result.pvalue == pytest.approx(0.2541582765202598)
 
 def test_proportion_analyze_norm_no_corr(
@@ -233,7 +260,51 @@ def test_proportion_analyze_norm_no_corr(
     metric = tea_tasting.metrics.proportion.Proportion(
         "has_order", method="norm", correction=False)
     result = metric.analyze(data_aggr, 0, 1, variant="variant")
+    assert result.effect_size_ci_lower == pytest.approx(-0.26508014358420723)
+    assert result.effect_size_ci_upper == pytest.approx(0.10129050087043773)
+    assert result.rel_effect_size_ci_lower == pytest.approx(-0.5647346534627081)
+    assert result.rel_effect_size_ci_upper == pytest.approx(0.367670355686605)
     assert result.pvalue == pytest.approx(0.3809096823118986)
+
+
+def test_proportion_analyze_norm_corr_widens_ci(
+    data_aggr: dict[object, tea_tasting.aggr.Aggregates],
+) -> None:
+    metric_corr = tea_tasting.metrics.proportion.Proportion(
+        "has_order", method="norm", correction=True)
+    metric_no_corr = tea_tasting.metrics.proportion.Proportion(
+        "has_order", method="norm", correction=False)
+    result_corr = metric_corr.analyze(data_aggr, 0, 1, variant="variant")
+    result_no_corr = metric_no_corr.analyze(data_aggr, 0, 1, variant="variant")
+    assert result_corr.effect_size_ci_lower < result_no_corr.effect_size_ci_lower
+    assert result_corr.effect_size_ci_upper > result_no_corr.effect_size_ci_upper
+    assert (
+        result_corr.rel_effect_size_ci_lower <
+        result_no_corr.rel_effect_size_ci_lower
+    )
+    assert (
+        result_corr.rel_effect_size_ci_upper >
+        result_no_corr.rel_effect_size_ci_upper
+    )
+
+
+def test_proportion_analyze_norm_ci_bounds_limited() -> None:
+    metric = tea_tasting.metrics.proportion.Proportion(
+        "has_order",
+        method="norm",
+        correction=False,
+    )
+    data = {
+        0: tea_tasting.aggr.Aggregates(count_=20, mean_={"has_order": 0.05}),
+        1: tea_tasting.aggr.Aggregates(count_=20, mean_={"has_order": 1.0}),
+    }
+    result = metric.analyze(data, 0, 1, variant="variant")  # type: ignore
+    assert result.effect_size_ci_lower == pytest.approx(0.640490098153452)
+    assert result.effect_size_ci_upper == 1
+
+    result = metric.analyze(data, 1, 0, variant="variant")  # type: ignore
+    assert result.effect_size_ci_lower == -1
+    assert result.effect_size_ci_upper == pytest.approx(-0.640490098153452)
 
 
 def test_sample_ratio_init_default() -> None:
